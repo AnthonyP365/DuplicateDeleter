5ac56f7677841f53432adeabd26bbb94b17d7655
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-04-07 19:11:30 -0400

    finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,54 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int index = 0;
+        Integer[] result = new Integer[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            int duplicates = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    duplicates++;
+                }
+            }
+            if (duplicates < maxNumberOfDuplications) {
+                result[index] = array[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(result, index);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int index = 0;
+        Integer[] result = new Integer[array.length];
+
+        for (int i = 0; i < array.length; i++) { // Iterate through given array and count the duplicates of the given number
+            int duplicates = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    duplicates++;
+                }
+            }
+            if (duplicates != exactNumberOfDuplications) {
+                result[index] = array[i];
+                index++;
+                }
+        }
+        return Arrays.copyOf(result, index);
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,55 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int index = 0;
+        String[] result = new String[array.length];
+
+// Iterates through the array and counts the number of duplicates
+        for (int i = 0; i < array.length; i++) {
+            int duplicates = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    duplicates++;
+                }
+            }
+// If the amount of duplicates for an element are lower than the max allowed, add element to new array and increment the new array's index
+            if (duplicates < maxNumberOfDuplications) {
+                result[index] = array[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(result, index);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int index = 0;
+        String[] result = new String[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            int duplicates = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    duplicates++;
+                }
+            }
+            if (duplicates != exactNumberOfDuplications) {
+                result[index] = array[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(result, index);
+    }
 }
